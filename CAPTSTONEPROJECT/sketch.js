c206bfda23bd60f30a41fa65039f753260abbb1e
okbuddychicanery <145151120+okbuddychicanery@users.noreply.github.com>
2023-12-13 12:11:29 -0600

    fixed gameover and killenemies. added mousepress to kill and more graphics
    mousepressed still needs works to add indepdent lmb and rmb clicks, after that ill work on time and spawn delays

--- CAPTSTONEPROJECT/sketch.js
+++ CAPTSTONEPROJECT/sketch.js
@@ -6,14 +6,15 @@
 let playerHP = 20;
 let rightside = [];
 let leftside = [];
+let killcount = 0;
 
 
 function setup() {
   createCanvas(1000, 1000);
-  for(let i =0; i < 2; i++){
+  for(let i =0; i < 7; i++){
     leftside.push(new Enemy(0, 0, height/2, 0, 20, 2));
   }
-  for(let i =0; i < 2; i++){
+  for(let i =0; i < 7; i++){
     rightside.push(new Enemy(0, width, height/2, 1, 20, 2));
   } 
 }
@@ -22,21 +23,30 @@ function draw() {
   background(220);
   drawTerrain();
   destroyEnemies();
-  if(playerHP ===0){
+  if(playerHP ===0){  
     gameOver();
   }
-  for(let rs of rightside){
-    rs.action();
+  else{
+    for(let rs of rightside){
+      rs.action();
+    }
+    for(let ls of leftside){
+      ls.action();
+    }
   }
-  for(let ls of leftside){
-    ls.action();
-  }
+  
 }
 
 
 function drawTerrain(){ // creates terrain for Player + npcs to exist on
   strokeWeight(2);
   rect(0,height/2, 1000, height*0.6);
+
+  fill(255);
+  rect(300, width/2, 400, 20);  // zone to kill enemies
+  fill(0,0,0);
+  text(str(playerHP), width/2, height*0.3);
+  text(str(killcount), width*0.2, height*0.3);
 }
 
 class Enemy{  // base class for enemy npcs
@@ -45,8 +55,8 @@ class Enemy{  // base class for enemy npcs
     this.type = type;
     this.x = x;
     this.y = y;
-    this.dir = dir; // moving direction
-    this.size = size;
+    this.dir = dir; // moving direction 
+    
     this.speed = speed;
     this.alive = true;
   }
@@ -57,9 +67,9 @@ class Enemy{  // base class for enemy npcs
     fill(0,0,0);
     if(this.type===0){  // basic 1hp guy
       fill(0,255,0);
-      circle(this.x,this.y, this.size);
+      circle(this.x,this.y-10, 15);
       fill(255,0,0);
-      rect(this.x, this.y-30, this.size, this.y*0.2); // hp block
+      rect(this.x-10, this.y, 20, 20); // hp block
       fill(0,0,0);
 
     }
@@ -97,13 +107,13 @@ class Enemy{  // base class for enemy npcs
 
 function destroyEnemies(){ // iterates through arrays splicing dead enemies
   for(let i = 0; i <rightside.length; i++){
-    if(Enemy.alive === false){
+    if(rightside[i].alive === false){
       rightside.splice(i,1);
     }
     
   }
   for(let i = 0; i <leftside.length; i++){
-    if(Enemy.alive === false){
+    if(leftside[i].alive === false){
       leftside.splice(i,1);
     }
     
@@ -111,12 +121,23 @@ function destroyEnemies(){ // iterates through arrays splicing dead enemies
 }
 
 function gameOver(){  // kills all enemies, display game over text
-  for(let i = 0; i <rightside.length; i++){
-    rightside.splice(i,1);
-  }
-  for(let i = 0; i <leftside.length; i++){
-    leftside.splice(i,1);
-  }
+  rightside =[];
+  leftside = [];
   textSize(20);
   text("GAME OVER", width/2, height/2);
+}
+
+function mousePressed(){  // fix later - add RMB for rightside
+  for(let i = 0; i <rightside.length; i++){
+    if(rightside[i].x < 700){
+      rightside.splice(i,1);
+      killcount+=1;
+    }
+  }
+  for(let i = 0; i <leftside.length; i++){
+    if(leftside[i].x > 300){
+      leftside.splice(i,1);
+      killcount+=1;
+    }
+  }
 }
